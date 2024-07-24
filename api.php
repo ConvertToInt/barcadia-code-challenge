2eaff0843a16e9d6d64b01f2d8a533127f1b19bc
ConvertToInt <tdalgleish7@gmail.com>
2024-07-24 11:59:29 +0100

    API updates, comments

--- api.php
+++ api.php
@@ -1,55 +1,52 @@
 <?php
+
+// Roman numerals map
+$numeralsMap = ['M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400, 'C' => 100, 'XC' => 90, 'L' => 50, 'XL' => 40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1];
+
+function convertIntToNumerals(array $numeralsMap, int $inputValue): string {
+    $result = '';
+    $inputValueInt = intval($inputValue);
+
+    foreach ($numeralsMap as $numeral => $number) {
+        // Calculate how many times the current numeral can fit into the input value
+        $matches = intval($inputValueInt / $number);
+        // Append the current numeral to the result string, based on how many matches are found
+        $result .= str_repeat($numeral, $matches);
+        // Update the input value by subtracting the total value of the appended numerals
+        $inputValueInt = $inputValueInt % $number;
+    }
+
+    return $result;
+}
+
+function convertNumeralsToInt(array $numeralsMap, string $inputValue): int {
+    $result = 0;
+
+    foreach ($numeralsMap as $numeral => $number) {
+        // Check if the input string starts with the current Roman numeral
+        while (strpos($inputValue, $numeral) === 0) {
+            // Add the value of the current Roman numeral to the result
+            $result += $number;
+            // Remove the current Roman numeral from the start of the input string
+            $inputValue = substr($inputValue, strlen($numeral));
+        }
+    }
+
+    return $result;
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Decode the request
     $data = json_decode(file_get_contents('php://input'), true);
     $inputValue = $data['inputValue'];
 
-    $numeralsMap = [
-        'M' => 1000, 
-        'CM' => 900, 
-        'D' => 500, 
-        'CD' => 400, 
-        'C' => 100, 
-        'XC' => 90, 
-        'L' => 50, 
-        'XL' => 40, 
-        'X' => 10, 
-        'IX' => 9, 
-        'V' => 5, 
-        'IV' => 4, 
-        'I' => 1
-    ];
-    
-    function convertIntToNumerals($numeralsMap, $inputValue) {
-        $inputValueInt = intval($inputValue);
-        $result = '';
-
-        foreach ($numeralsMap as $numeral => $number) {
-            $matches = intval($inputValueInt / $number);
-            $result .= str_repeat($numeral, $matches);
-            $inputValueInt = $inputValueInt % $number;
-        }
-
-        return $result;
-    }
-
-    function convertNumeralsToInt($numeralsMap, $inputValue) {
-        $result = 0;
-
-        foreach ($numeralsMap as $key => $value) {
-            while (strpos($inputValue, $key) === 0) {
-                $result += $value;
-                $inputValue = substr($inputValue, strlen($key));
-            }
-        }
-
-        return $result;
-    }
-
+    // Determine whether to convert to integer, or to roman numerals
     if (is_numeric($inputValue)) {
         $convertedValue = convertIntToNumerals($numeralsMap, $inputValue);
     } elseif (is_string($inputValue)) {
         $convertedValue = convertNumeralsToInt($numeralsMap, $inputValue);
     }
 
+    // Return the encoded converted answer
     echo json_encode(['convertedValue' => $convertedValue]);
 }
